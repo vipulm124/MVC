2437af1f787b1f2eb900ef53d24033fe07cf0c00
Vipul Malhotra <vipulm124@hotmail.com>
2018-06-26 23:07:51 +0530

    new project and branch

---
+++ MVCCustomLogin/MVCCustomLogin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCCustomLogin.Startup))]
+namespace MVCCustomLogin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
